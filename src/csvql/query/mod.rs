9a5854fe9cdfa85b97c1c528e6f923138fea5fcb
Brian W. Wolter <brianwolter@gmail.com>
2023-01-15 14:27:20 -0600

    ...

--- src/csvql/query/mod.rs
+++ src/csvql/query/mod.rs
@@ -1,2 +1,39 @@
+use std::io::Read;
 
-struct _Query {}
+use csv;
+
+struct Query<R: Read> {
+  sources: Vec<Source<R>>,
+  select: Vec<Selector<R>>,
+}
+
+struct Frame<R: Read> {
+  data: R,
+}
+
+impl<R: Read> Frame<R> {
+  fn new<'a>(data: &'a mut R) -> Frame<&'a mut R> {
+    Frame{
+      data: data,
+    }
+  }
+  
+  fn rows(&mut self) {
+    let _ = csv::Reader::from_reader(&mut self.data).records();
+  }
+}
+
+struct Source<R: Read> {
+  name: String,
+  data: Frame<R>,
+}
+
+struct Selector<R: Read> {
+  columns: Vec<Column<R>>,
+}
+
+struct Column<R: Read> {
+  source: Source<R>,
+  column: String,
+  index: u32,
+}
