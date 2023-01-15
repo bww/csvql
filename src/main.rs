f536d1288d78dd706a690f205f99ddc91e877218
Brian W. Wolter <brianwolter@gmail.com>
2023-01-15 14:30:02 -0600

    ...

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,13 @@ use clap::Parser;
 pub struct Options {
   #[clap(long, help="Enable debugging mode")]
   pub debug: bool,
-  #[clap(long, help="Enable alternate screen debugging mode (no switch on exit)")]
-  pub debug_alternate: bool,
-  #[clap(long, help="Enable editor debugging mode; additional frames are not displayed")]
-  pub debug_editor: bool,
   #[clap(long)]
   pub verbose: bool,
   #[clap(help="Document to open")]
-  pub doc: Option<String>,
+  pub doc: Vec<String>,
 }
 
 fn main() {
-  println!("Hello, world!");
+  let opts = Options::parse();
+  println!("Hello, world! {:?}", opts);
 }
